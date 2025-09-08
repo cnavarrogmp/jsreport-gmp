33f1220a1d66d1f31ce43393810d3f41bf00eccc
Carmen Navarro <cnavarro@grupompleo.com>
2025-09-08 19:17:32 +0200

    Fix: Use `template.shortName` instead of `template.fullPath` in `test-template-interactive.js`; append new error log entries in `profiles` data.

--- test-template-interactive.js
+++ test-template-interactive.js
@@ -156,7 +156,7 @@ async function generatePDF(template, testData) {
   return new Promise((resolve, reject) => {
     const requestPayload = JSON.stringify({
       template: {
-        name: template.fullPath
+        name: template.shortName
       },
       data: testData,
       options: {
